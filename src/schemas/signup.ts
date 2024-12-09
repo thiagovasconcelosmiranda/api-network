2c6a7defe3643e9b32194156b3e880348b140f84
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2024-12-09 10:14:30 -0300

    Nova atualização

--- src/schemas/signup.ts
+++ src/schemas/signup.ts
@@ -2,6 +2,6 @@ import {z} from 'zod';
 
 export const signupSchema = z.object({
    name: z.string().min(0, 'Name é obrigatório'),
-   email: z.string().min(2, 'Email invalido'),
-   password: z.string().min(4, 'Pelo menos 4 caracteres')
+   email: z.string({message: 'E-mail é obrigatório'}).email('E-mail invalido'),
+   password: z.string({message: 'Senha é obrigatório'}).min(4, 'Precisa ter 4 ou mais caracteres')
 });
